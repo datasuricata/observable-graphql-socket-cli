97c83eff7e32dab5b64c6b6ea262bd6ec6fcde9c
lucas.moraes.dev@gmail.com <lucas.moraes.dev@gmail.com>
2021-06-01 21:08:19 -0300

    spike

---
+++ graphql-subscription-poc/CategoryConsumer.cs
@@ -0,0 +1,49 @@
+﻿using GraphQL;
+using GraphQL.Client.Abstractions;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Reactive.Linq;
+
+namespace graphql_subscription_poc
+{
+    public class CategoryConsumer
+    {
+        private readonly IGraphQLClient _client;
+        private readonly ILogger<CategoryConsumer> _logger;
+        public CategoryConsumer(IGraphQLClient client, ILogger<CategoryConsumer> logger)
+        {
+            _client = client;
+            _logger = logger;
+        }
+
+        public void Subscribe()
+        {
+            var categoryJoinedRequest = new GraphQLRequest
+            {
+                Query = @"
+                    subscription{
+                      onCategoryCreate{
+                        active
+                        createdAt
+                        id
+                        name
+                        priority
+                      }
+                    }"
+            };
+
+            IObservable<GraphQLResponse<dynamic>> stream =
+                _client.CreateSubscriptionStream<dynamic>(categoryJoinedRequest);
+
+            IDisposable subscription = stream.Subscribe(response =>
+            {
+                _logger.LogInformation((string)JsonConvert.SerializeObject(response.Data));
+            });
+
+            while (true) { }
+
+            //subscription.Dispose();
+        }
+    }
+}
---
+++ graphql-subscription-poc/Program.cs
@@ -0,0 +1,58 @@
+﻿using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
+using GraphQL.Client.Serializer.Newtonsoft;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace graphql_subscription_poc
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var host = CreateHost();
+            await host.StartAsync();
+
+            var consumer = host.Services.GetService<CategoryConsumer>();
+            var log = host.Services.GetService<ILogger<Program>>();
+
+            try
+            {
+                consumer.Subscribe();
+            }
+            catch (Exception e)
+            {
+                log.LogError(e, "falha na integração com graphql server");
+            }
+        }
+
+        private static IHost CreateHost()
+        {
+            var builder = new HostBuilder();
+
+            builder.ConfigureAppConfiguration((context, builder) =>
+            {
+                builder.Sources.Clear();
+                builder
+                .SetBasePath(AppContext.BaseDirectory)
+                .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables();
+            });
+
+            builder.ConfigureServices((context, builder) =>
+            {
+                var config = context.Configuration;
+
+                builder.AddLogging(c => c.AddConsole()).Configure<LoggerFilterOptions>(cfg => cfg.MinLevel = LogLevel.Debug);
+                builder.AddScoped<IGraphQLClient>(s => new GraphQLHttpClient(config["GraphQLURI"], new NewtonsoftJsonSerializer()));
+                builder.AddScoped<CategoryConsumer>();
+            });
+
+            return builder.Build();
+        }
+    }
+}
